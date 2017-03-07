bab8d9e4f608c4104c9ff395e9fee8c1431ca73b
Tom Burnett <t_burnett@outlook.com>
2017-03-07 20:49:53 +0000

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose = require('./db/mongoose');
+
+const Todo = require('./models/Todo');
+const User = require('./models/User');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+  todo.save()
+    .then(doc => res.send(doc))
+    .catch(err => res.status(400).send(err));
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
